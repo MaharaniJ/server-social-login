695baacae5011a77ec1f7536d4c0e6cf2bfae901
MaharaniJ <maharani.jeyaraman@outlook.com>
2023-07-11 16:16:18 +0530

    dotenv

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./config/db");
 const cookieParser = require('cookie-parser')
-
+const dotenv = require('dotenv');
 //load env variables
 require("dotenv").config();
 
